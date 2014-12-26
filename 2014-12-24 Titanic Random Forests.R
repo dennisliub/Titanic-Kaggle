9c827b0c50c84849f420d6e7f50eaccf5ab9edea
Dennis Lyubyvy <dennis.lyubyvy@gmail.com>
2014-12-26 15:16:28 -0500

    history check

--- 2014-12-24 Titanic Random Forests.R
+++ 2014-12-24 Titanic Random Forests.R
@@ -2,15 +2,15 @@
 # Finished by Dennis Lyubyvy 
 
 # Set working directory and import datafiles
-setwd("~/Desktop/Titanic Kaggle/")
+setwd("~/github/Titanic-Kaggle/")
 train <- read.csv("train.csv")
 test <- read.csv("test.csv")
 
 # Install and load required packages for decision trees and forests
 library(rpart)
-install.packages('randomForest')
+#install.packages('randomForest')
 library(randomForest)
-install.packages('party')
+#install.packages('party')
 library(party)
 
 # Join together the test and train sets for easier feature engineering
@@ -50,22 +50,25 @@ summary(combi$Age)
 Agefit <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + FamilySize, 
                 data=combi[!is.na(combi$Age),], method="anova")
 
+#Agefit <- cforest(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + FamilySize, 
+#                data=combi[!is.na(combi$Age),], controls=cforest_unbiased(ntree=2000, mtry=3))
+
+
 combi$Age[is.na(combi$Age)] <- predict(Agefit, combi[is.na(combi$Age),])
 
-combi$Age_Pclass <- log(combi$Age)*combi$Pclass
-combi$Sex_Pclass <- as.numeric(combi$Sex)*combi$Pclass
 # Check what else might be missing
 summary(combi)
 # Fill in Embarked blanks
 summary(combi$Embarked)
 which(combi$Embarked == '')
 combi$Embarked[c(62,830)] = "S"
+#combi$Embarked[c(62,830)] = "C"
 combi$Embarked <- factor(combi$Embarked)
 # Fill in Fare NAs
 summary(combi$Fare)
 which(is.na(combi$Fare))
+#combi$Fare[1044] <- median(combi$Fare[which(combi$Pclass==3)], na.rm=TRUE)
 combi$Fare[1044] <- median(combi$Fare, na.rm=TRUE)
-
 #log Age (Dennis)
 #combi$LogAge <- log(combi$Age)
 combi$Sector <- substr(combi$Cabin, 1, 1)
@@ -80,6 +83,33 @@ combi$FamilyID2[combi$FamilySize <= 3] <- 'Small'
 # And convert back to factor
 combi$FamilyID2 <- factor(combi$FamilyID2)
 
+combi$Ticket <- as.factor(combi$Ticket)
+tickets <- unique(combi$Ticket)
+for (t in tickets) {
+    who <- which 
+    print(t)
+    
+}
+
+#PCA
+str(combi)
+
+combi_pca <- combi
+str(combi_pca)
+combi$Sex <- as.numeric(combi$Sex)
+combi$Sex <- as.numeric(combi$Sex)
+
+
+pca_data <- prcomp(combi_pca, scale=TRUE)
+apply(combi_pca , 2, mean)
+str(combi_pca)
+
+summary(pca_data)
+plot(pca_data,type="lines")
+loadings(pca_data)
+pca_data$scores
+combi$PCA1 <- pca_data$scores[,1]
+combi$PCA2 <- pca_data$scores[,2]
 #RANDOM FORESTS
 
 # Split back into test and train sets
@@ -119,7 +149,10 @@ for (i in 1:5) {
                        Title + 
                        FamilySize + 
                        FamilyID +
-                       Sector
+                       Sector 
+#                      + Ticket
+#                      + PCA1
+#                       PCA2
 ,data = train_fold_data, controls=cforest_unbiased(ntree=2000, mtry=3))
 
 train_pred <- predict(fit, train_fold_data, OOB=TRUE, type = "response")
@@ -140,10 +173,18 @@ fit <- cforest(as.factor(Survived) ~ Pclass +
                    Title + 
                    FamilySize + 
                    FamilyID +
-                   Sector
+                   Sector 
+               #+
+                   #PCA1 +
+                   #PCA2
                ,data = train, controls=cforest_unbiased(ntree=2000, mtry=3))
 
 # Now let's make a prediction and write a submission file
 Prediction <- predict(fit, test, OOB=TRUE, type = "response")
 submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "forest.csv", row.names = FALSE)
+
+
+# CHECK
+
+
